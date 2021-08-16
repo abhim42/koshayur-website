354cf18b80b4d32d3a73297248b1ef1fc535605c
abhim42 <abhilashamishra777@gmail.com>
2021-08-16 12:10:26 +0530

    minor commit

--- js/swiper-initialize-script.js
+++ js/swiper-initialize-script.js
@@ -1,31 +1,31 @@
- var swiper = new Swiper(".mySwiper", {
-      autoSlide: true,
-      slidesPerView: 5,
-      spaceBetween: 30,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-      autoplay: {
-        delay: 5000,
-      },
+var swiper = new Swiper(".mySwiper", {
+  autoSlide: true,
+  slidesPerView: 5,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 5000,
+  },
 
-      // Responsive breakpoints
-      breakpoints: {
-        // when window width is >= 320px
-        320: {
-          slidesPerView: 2,
-          spaceBetween: 20
-        },
-        // when window width is >= 480px
-        480: {
-          slidesPerView: 3,
-          spaceBetween: 30
-        },
-        // when window width is >= 640px
-        640: {
-          slidesPerView: 4,
-          spaceBetween: 40
-        }
-      }
-    });
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+  },
+});
